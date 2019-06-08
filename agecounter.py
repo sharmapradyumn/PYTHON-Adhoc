540c3ef23298ffc1cd06aa35c00a40947bc9ed23
sharmapradyumn <47988611+sharmapradyumn@users.noreply.github.com>
2019-06-08 13:27:02 +0530

    this will tell you when you turn 99

---
+++ agecounter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import datetime
+user_name=input("Enter your name:-")
+user_age=int(input("Enter your age:-"))
+
+# storing value of current date w.r.t year,month and day
+current_datetime=datetime.datetime.now()
+current_year=current_datetime.year
+new_year=current_year-user_age+95
+print("Hey "+str(user_name)+" you will turn 95 years old in "+str(new_year))
